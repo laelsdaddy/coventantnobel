db7c3ba44e25f0ba5c5af56adaaf4c3b2f2a0ae7
Chieloka Ifionu <cedar.innovations@gmail.com>
2020-06-09 23:17:51 +0100

    june update

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    const inputFocus = document.querySelector('.contact-form-input').focus();
+    if(inputFocus){
+        document.querySelector('.contact-icon-wrap').style.border = '2px solid purple';
+    };
